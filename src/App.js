8161d6ed916b0522ae843af4862cef713ab7f005
Phạm Trung Tuyên <phamtrungtuyen@MacBook-Pro-cua-Pham.local>
2021-10-28 22:28:18 +0700

    Add file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+import ECommerce from "./components/ECommerce";
+
+function App() {
+  return (
+    <div className="App">
+      <ECommerce />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ECommerce.js
@@ -0,0 +1,397 @@
+import * as React from "react";
+import Rating from "@mui/material/Rating";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../css/eCommerce.css";
+
+const ECommerce = () => {
+  return (
+    <div className="product">
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-1.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-2.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={2.5} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-3.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-4.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-5.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-6.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-7.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-8.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-9.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-10.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-11.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card-all">
+        <div className="card-product">
+          <div className="images-product">
+            <img src="images/product-12.png" alt="" />
+            <ul className="actions">
+              <li>
+                <i class="fas fa-heart"></i>
+                <span>Add to Wishlist</span>
+              </li>
+              <li>
+                <i class="fas fa-shopping-cart"></i>
+                <span>Add to Card </span>
+              </li>
+              <li>
+                <i class="fas fa-eye"></i>
+                <span>View Details</span>
+              </li>
+            </ul>
+          </div>
+          <div className="card-content">
+            <div className="name-product">
+              <h3>Nokia</h3>
+            </div>
+            <div className="prince-rating">
+              <div className="prince">$ 399</div>
+              <div className="rating">
+                <Rating value={3} readOnly size="small" precision={0.5} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ECommerce;
